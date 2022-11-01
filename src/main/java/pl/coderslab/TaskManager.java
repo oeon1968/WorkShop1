7641bd7aa4095373c90848ad58ec26bc23e43a9d
oeon1968 <oeon1968@gmail.com>
2022-11-01 13:31:16 +0100

    still working on getTasks

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,13 +1,17 @@
 package pl.coderslab;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import org.apache.commons.lang3.ArrayUtils;
+
 
 public class TaskManager {
     public static void main(String[] args) {
         File myFile = new File("tasks.csv");
         String[] listOfAction = {"Add","Remove", "List", "Exit"};
         String action="OTHER";
+        String[][] listOfTask  = getTasks(myFile);
         while (!"EXIT".equals(action.toUpperCase())){
             action =showActions(listOfAction);
             switch (action.toUpperCase()){
@@ -24,6 +28,40 @@ public class TaskManager {
             }
         }
 
+    }
+    private static String[][] getTasks(File fileName){
+
+        try {
+            int numberOfLine = 0;
+            int numberOfField = 0;
+            Scanner scFile = new Scanner(fileName);
+            String[][] myListOfTasks;
+            while(scFile.hasNextLine()){
+                numberOfLine++;
+                numberOfField= scFile.nextLine().split(",").length;
+            }
+            scFile.close();
+            scFile = new Scanner(fileName);
+            myListOfTasks = new String[numberOfLine][numberOfField];
+            while (scFile.hasNextLine()){
+                for(int i=0; i < numberOfLine; i++){
+                    String[] myLine;
+                    myLine = scFile.nextLine().split(",");
+                    for (int j=0; j< myLine.length; j++){
+                        myListOfTasks[i][j] = myLine[j];
+                    }
+
+                }
+
+
+            }
+
+        } catch (FileNotFoundException exFNF){
+            System.out.println("File "+fileName+" not found!");
+            exFNF.getStackTrace();
+            return null;
+        }
+        return myListOfTask;
     }
     private static String showActions(String[] listOfAction){
         Scanner chooseAction = new Scanner(System.in);
